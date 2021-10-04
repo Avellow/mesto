b77e9004e9c2012e500e3b6ff088948c4ecea619
Avellow <avellow@yandex.ru>
2021-10-04 10:22:01 +0300

    исправить замечания

--- scripts/index.js
+++ scripts/index.js
@@ -16,45 +16,22 @@ const popupCloseBtn = popupElement.querySelector('.popup__close-button'); //кн
 
 //напишу необходимые функции
 function openPopup() {                           //функция открывающая попап
-  popupElement.classList.add('popup__opened');
+  popupElement.classList.add('popup_opened');
 }
 function closePopup() {                          //функция закрывающая попап
-  popupElement.classList.remove('popup__opened');
-}
-
-function getProfileName() {                     //функция возвращающая текущее имя профия
-  return profileNameElement.textContent;
-}
-function getProfileJob() {                          //функция возвращающая текущую работу профиля
-  return profileJobElement.textContent;
-}
-function postProfileName(name) {                  //функция устанавливает переданное имя профилю
-  profileNameElement.textContent = name;
-}
-function postProfileJob(job) {                  //функция устанавливает переданное место работы профилю
-  profileJobElement.textContent = job;
-}
-
-function changeLikeState(el) {                         //ф-ция меняет состояние лайка
-  el.classList.toggle('place__like-button_active');
+  popupElement.classList.remove('popup_opened');
 }
 
 function profileEditHandler() {               //функция хендлер сработающая при нажатии на кнопку edit
   openPopup();
-  nameInput.value = getProfileName();
-  jobInput.value = getProfileJob();
+  nameInput.value = profileNameElement.textContent;
+  jobInput.value = profileJobElement.textContent;
 }
 function formSubmitHandler(evt) {           // функция хендлер сработающая при сохранении формы
   evt.preventDefault();
-  let newProfileName = nameInput.value;
-  let newProfileJob = jobInput.value;
-  postProfileName(newProfileName);
-  postProfileJob(newProfileJob);
+  profileNameElement.textContent = nameInput.value;
+  profileJobElement.textContent = jobInput.value;
   closePopup();
-}
-
-function likeChangeHandler(evt) {
-  changeLikeState(evt.target);
 }
 
 //реализую функционал кнопок через event listener
@@ -63,7 +40,3 @@ popupCloseBtn.addEventListener('click', closePopup);
 
 formElement.addEventListener('submit', formSubmitHandler);
 
-//применю для каждого элемента массива указанный handler
-placeLikeBtns.forEach((placeLikeBtn) => {                           //надо бы почитать побольше о стрелочных функциях
-  placeLikeBtn.addEventListener('click', likeChangeHandler);
-});
