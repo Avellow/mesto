79f0d93ff9a8b0b41317d4260c2e14ff8e6fd8b2
Avellow <avellow@yandex.ru>
2021-11-29 17:04:35 +0300

    feature сделать класс Section

---
+++ scripts/Section.js
@@ -0,0 +1,16 @@
+//отвечает за отрисовку элементов
+export default class Section {
+  constructor({ items, renderer }, containerSelector) {
+    this._items = items;
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
+  }
+
+  renderItems() {
+    this._items.forEach(this._renderer);
+  }
+
+  addItem(el) {
+    this._container.append(el);
+  }
+}
